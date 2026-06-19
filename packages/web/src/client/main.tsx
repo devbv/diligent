6912abf76e1e326f05fb4080a54bd210117a10c8
jjss <jjss@jjssui-MacBookPro.local>
2026-06-19 14:35:43 +0900

    chore(web): drop message list polyfill imports

--- packages/web/src/client/main.tsx
+++ packages/web/src/client/main.tsx
@@ -1,9 +1,4 @@
 // @summary React entrypoint mounting the Web CLI app and loading Tailwind/token styles
-import "core-js/actual/array/at";
-import "core-js/actual/array/find-last";
-import "core-js/actual/array/find-last-index";
-import "core-js/actual/object/has-own";
-import "core-js/actual/string/at";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
